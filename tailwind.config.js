76a252f687d98fcf91c88173fb1861ccebf0d608
lukpaikat <mhasbisaputra5@gmail.com>
2022-05-04 08:12:25 +0800

    add 44px min width and height

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
-    colors: {
-      'county-green': '#093523',
-      'bright-red': '#EA0801',
-      soapstone: '#FDFDFD'
-    }
+    extend: {
+      colors: {
+        'county-green': '#093523',
+        'bright-red': '#EA0801',
+        soapstone: '#FDFDFD',
+      },
+      minWidth: {
+        '44px': '44px',
+      },
+      minHeight: {
+        '44px': '44px',
+        '410px': '410px',
+      },
+    },
   },
   plugins: [],
   darkMode: 'class',
-}
+};
