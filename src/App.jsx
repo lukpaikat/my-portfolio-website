a1bb66a5fef7809ef5ec54a127889c39178c639b
lukpaikat <mhasbisaputra5@gmail.com>
2022-05-06 03:32:29 +0800

    add placeholder sections

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,16 @@ function App() {
   return (
     <div>
       <NavBar />
-      <h1 className="mt-20 text-3xl font-bold underline h-screen">
+      <h1 id="hero" className="mt-20 text-3xl font-bold underline h-screen">
         Hello world!
       </h1>
-      <h1 className="mt-20 text-3xl font-bold underline h-screen">
+      <h1 id="about" className="mt-20 text-3xl font-bold underline h-screen">
+        Hello world!
+      </h1>
+      <h1 id="skills" className="mt-20 text-3xl font-bold underline h-screen">
+        Hello world!
+      </h1>
+      <h1 id="work" className="mt-20 text-3xl font-bold underline h-screen">
         Hello world!
       </h1>
     </div>
