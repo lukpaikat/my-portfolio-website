36db7a1a4a089322133e3e3768b08998b78dacc1
lukpaikat <mhasbisaputra5@gmail.com>
2022-05-06 04:42:14 +0800

    add hero ccomponent

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+  // hero here
+  return (
+    <section id="hero" className="mt-20 text-3xl font-bold underline h-screen">
+      <h1>this is hero section</h1>
+    </section>
+  );
+}
+
+export default Hero;
