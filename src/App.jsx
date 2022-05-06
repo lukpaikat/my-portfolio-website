edb307d239184b512e2382498a8015d145269d6c
lukpaikat <mhasbisaputra5@gmail.com>
2022-05-06 08:10:36 +0800

    add main tag

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Hero />
-      <h1 id="about" className="mt-20 text-3xl font-bold underline h-screen">
-        Hello world!
-      </h1>
-      <h1 id="skills" className="mt-20 text-3xl font-bold underline h-screen">
-        Hello world!
-      </h1>
-      <h1 id="work" className="mt-20 text-3xl font-bold underline h-screen">
-        Hello world!
-      </h1>
+      <main>
+        <Hero />
+        <h1 id="about" className="mt-20 text-3xl font-bold underline h-screen">
+          Hello world!
+        </h1>
+        <h1 id="skills" className="mt-20 text-3xl font-bold underline h-screen">
+          Hello world!
+        </h1>
+        <h1 id="work" className="mt-20 text-3xl font-bold underline h-screen">
+          Hello world!
+        </h1>
+      </main>
     </div>
   );
 }
