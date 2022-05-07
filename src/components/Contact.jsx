8c3fbd3c55dee9c30dd07852ec2832638622e13f
lukpaikat <mhasbisaputra5@gmail.com>
2022-05-08 07:00:50 +0800

    add placeholder components

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section id="contact" className="h-screen max-h-[720px]">
+      <h2 className="mb-4 text-7xl 2xl:text-8xl md:text-center">This is contact Section</h2>
+    </section>
+  );
+}
+
+export default Contact;
---
+++ src/components/Work.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Work() {
+  return (
+    <section id="work" className="h-screen max-h-[720px]">
+      <h2 className="mb-4 text-7xl 2xl:text-8xl md:text-center">This is Work Section</h2>
+    </section>
+  );
+}
+
+export default Work;
