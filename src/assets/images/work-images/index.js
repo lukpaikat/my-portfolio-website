354e7141663cb37e89ad4c76554432d37c81d26c
lukpaikat <mhasbisaputra5@gmail.com>
2022-05-08 09:51:45 +0800

    add work images and its data

---
+++ src/assets/images/work-images/index.js
@@ -0,0 +1,35 @@
+import bookShelfImage from './bookshelf-lukpaikat.netlify.app_ 720.png';
+import justWeatherImage from './justweather-lukpaikat.netlify.app_.png';
+import etchASketchImage from './lukpaikat.github.io_etch-a-sketch_.png';
+import rockPaperScrissorsImage from './lukpaikat.github.io_rock-paper-scissors_.png';
+import nyam2FoodsImage from './nyam2-foods.netlify.app_.png';
+
+const workImagesData = [
+  {
+    name: 'Nyam2Foods',
+    image: nyam2FoodsImage,
+    url: 'https://nyam2-foods.netlify.app/',
+  },
+  {
+    name: 'BookShelf',
+    image: bookShelfImage,
+    url: 'https://bookshelf-lukpaikat.netlify.app/',
+  },
+  {
+    name: 'JustWeather',
+    image: justWeatherImage,
+    url: 'https://justweather-lukpaikat.netlify.app/',
+  },
+  {
+    name: 'etch-a-sketch',
+    image: etchASketchImage,
+    url: 'https://lukpaikat.github.io/etch-a-sketch/',
+  },
+  {
+    name: 'Rock Paper Scissors',
+    image: rockPaperScrissorsImage,
+    url: 'https://lukpaikat.github.io/rock-paper-scissors/',
+  },
+];
+
+export default workImagesData;
