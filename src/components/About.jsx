458c17902626d2d5c87e0b477069444586298eef
lukpaikat <mhasbisaputra5@gmail.com>
2022-05-09 06:42:56 +0800

    add max width and contact me btn

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'react-scroll/modules/components/Link';
 import myPhoto from '../assets/images/my-photo-grayscale.JPG';
 
 function About() {
@@ -6,9 +7,10 @@ function About() {
     <section
       id="about"
       className="landscape:grid landscape:grid-cols-[45%_55%] landscape:h-screen landscape:max-h-[720px]
-      landscape:min-h-[440px] px-4 landscape:pl-0"
+      landscape:min-h-[440px] px-4 landscape:pl-0 max-w-[1750px]
+      mx-auto"
     >
-      <div className="flex items-center justify-center relative before:w-full before:h-[90%] landscape:before:h-full landscape:before:w-10/12 before:top-0 landscape:before:left-0 landscape:before:top-auto before:border landscape:before:border-l-0 before:border-spanish-green before:absolute max-h-[720px]">
+      <div className="flex items-center justify-center relative before:w-full before:max-w-[800px] before:h-[90%] landscape:before:h-full landscape:before:w-10/12 before:top-0 landscape:before:left-0 landscape:before:top-auto before:border before:border-spanish-green before:absolute max-h-[720px]">
         <img
           className="h-10/12 w-10/12 relative mt-4 landscape:mt-0
             landscape:w-11/12 landscape:h-11/12 max-h-[600px] max-w-[600px] aspect-square object-cover"
@@ -19,22 +21,39 @@ function About() {
       <article className="flex landscape:justify-center flex-col my-4 mx-6 xl:max-w-[90%]">
         <h2 className="mb-4 text-5xl xl:text-6xl 2xl:text-8xl 2xl:mb-12 text-center landscape:text-left">ABOUT</h2>
         <div className="max-w-lg landscape:max-w-none mx-auto">
-          <p className="mb-4 2xl:text-3xl 2xl:mb-8">
+          <p className="mb-4 2xl:text-2xl 2xl:mb-8">
             My name is Muhammad Hasbi Saputra. I’m from Indonesia.
             I like working with javaScript and other stuff related to front-end development.
             <small>Therefore I proclaim my self as a front-end developer.</small>
           </p>
-          <p className="mb-4 2xl:text-3xl 2xl:mb-14">
+          <p className="mb-4 2xl:text-2xl 2xl:mb-8">
             Currently I’m working on improving my skills and
             seeking entry level front-end development job
           </p>
-          <a
-            href="https://s3-ap-southeast-1.amazonaws.com/glints-dashboard/resume/7bde17d26c513c1379169e0f0161378a.pdf"
-            className="bg-county-green text-soapstone dark:bg-soapstone dark:text-county-green block w-fit
-            min-h-44px min-w-44px py-3 px-4 mr-2 my-2 cursor-pointer 2xl:text-xl 2xl:px-7 2xl:py-6"
-          >
-            My resume
-          </a>
+          <p className="mb-4 2xl:text-2xl 2xl:mb-8">
+            You can contact me if you&apos;re interested to offer me a job or a freelance work.
+          </p>
+          <div className="flex flex-wrap">
+            <Link
+              tabIndex={0}
+              to="contact"
+              smooth
+              duration={500}
+              offset={-200}
+              className="bg-county-green text-soapstone dark:bg-soapstone dark:text-county-green w-fit
+              min-h-44px min-w-44px py-3 px-4 my-2 cursor-pointer 2xl:text-2xl 2xl:px-7 2xl:py-6 mr-4 2xl:mr-8"
+            >
+              Contact me
+            </Link>
+            <a
+              href="https://s3-ap-southeast-1.amazonaws.com/glints-dashboard/resume/7bde17d26c513c1379169e0f0161378a.pdf"
+              className="text-county-green dark:text-soapstone w-fit
+              min-h-44px min-w-44px py-3 px-4 mr-2 my-2 cursor-pointer 2xl:text-2xl 2xl:px-7 2xl:py-6
+              border border-county-green dark:border-soapstone"
+            >
+              My resume
+            </a>
+          </div>
         </div>
 
       </article>
