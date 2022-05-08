3c5eb9a4b45a9f9d203e1c94b12d7c64083499f1
lukpaikat <mhasbisaputra5@gmail.com>
2022-05-09 02:24:30 +0800

    add code url

--- src/assets/images/work-images/index.js
+++ src/assets/images/work-images/index.js
@@ -10,30 +10,35 @@ const workImagesData = [
     description: 'A dummy restaurant catalogue',
     image: nyam2FoodsImage,
     url: 'https://nyam2-foods.netlify.app/',
+    codeUrl: 'https://gitfront.io/r/lukpaikat/ee2191cca7399d6c71abf246ede3ea5188962119/Nyam2-foods/',
   },
   {
     name: 'BookShelf',
     description: 'Book reading manager webapp',
     image: bookShelfImage,
     url: 'https://bookshelf-lukpaikat.netlify.app/',
+    codeUrl: 'https://gitfront.io/r/lukpaikat/9977b1958325696f728d9f000310b6baf54a04b8/bookshelf/',
   },
   {
     name: 'JustWeather',
     description: 'Weather forecast webapp',
     image: justWeatherImage,
     url: 'https://justweather-lukpaikat.netlify.app/',
+    codeUrl: 'https://gitfront.io/r/lukpaikat/8ec706a533347fdfee6ec86f19782a4171f8d5aa/JustWeather/',
   },
   {
     name: 'etch-a-sketch',
     description: 'pixel-by-pixel drawing webapp',
     image: etchASketchImage,
     url: 'https://lukpaikat.github.io/etch-a-sketch/',
+    codeUrl: 'https://github.com/lukpaikat/etch-a-sketch',
   },
   {
     name: 'Rock Paper Scissors',
     description: 'Rock Paper Scissors webapp game',
     image: rockPaperScrissorsImage,
     url: 'https://lukpaikat.github.io/rock-paper-scissors/',
+    codeUrl: 'https://github.com/lukpaikat/rock-paper-scissors',
   },
 ];
 
