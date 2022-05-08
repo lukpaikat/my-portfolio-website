decfff3f71152f0589b4a3cac084e4ba1ef66ba2
lukpaikat <mhasbisaputra5@gmail.com>
2022-05-08 11:38:08 +0800

    add description data

--- src/assets/images/work-images/index.js
+++ src/assets/images/work-images/index.js
@@ -7,26 +7,31 @@ import nyam2FoodsImage from './nyam2-foods.netlify.app_.png';
 const workImagesData = [
   {
     name: 'Nyam2Foods',
+    description: 'A dummy restaurant catalogue',
     image: nyam2FoodsImage,
     url: 'https://nyam2-foods.netlify.app/',
   },
   {
     name: 'BookShelf',
+    description: 'Book reading manager webapp',
     image: bookShelfImage,
     url: 'https://bookshelf-lukpaikat.netlify.app/',
   },
   {
     name: 'JustWeather',
+    description: 'Weather forecast webapp',
     image: justWeatherImage,
     url: 'https://justweather-lukpaikat.netlify.app/',
   },
   {
     name: 'etch-a-sketch',
+    description: 'pixel-by-pixel drawing webapp',
     image: etchASketchImage,
     url: 'https://lukpaikat.github.io/etch-a-sketch/',
   },
   {
     name: 'Rock Paper Scissors',
+    description: 'Rock Paper Scissors webapp game',
     image: rockPaperScrissorsImage,
     url: 'https://lukpaikat.github.io/rock-paper-scissors/',
   },
