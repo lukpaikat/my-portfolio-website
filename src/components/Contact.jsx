ecf1f8507ee91bb0f2191cc2e9c83688cae02fda
lukpaikat <mhasbisaputra5@gmail.com>
2022-05-11 10:37:28 +0800

    add labels for input form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,13 +8,45 @@ function Contact() {
         Submit the form below or send me an email - mhasbisaputra5@gmail.com
       </p>
       <form action="https://formsubmit.co/e53bda67b333f6316d7faf1f85f9aa43" method="POST" className="flex flex-col max-w-[650px] mx-auto 2xl:max-w-[980px] mb-8 2xl:mb-16">
-        <input className="my-4 mx-6 p-2 text-county-green 2xl:text-2xl 2xl:p-4 border border-county-green" type="text" name="name" placeholder="Name" required />
-        <input className="my-4 mx-6 p-2 text-county-green 2xl:text-2xl 2xl:p-4 border border-county-green" type="email" name="email" placeholder="email" required />
+        <label htmlFor="name" className="my-4 mx-6">
+          <span className="sr-only">Name</span>
+          <input
+            id="name"
+            className="p-2 text-county-green w-full
+              2xl:text-2xl 2xl:p-4 border border-county-green block"
+            type="text"
+            name="name"
+            placeholder="Name"
+            required
+          />
+        </label>
+        <label htmlFor="email" className="my-4 mx-6">
+          <span className="sr-only">Email</span>
+          <input
+            id="email"
+            className="p-2 text-county-green w-full
+              2xl:text-2xl 2xl:p-4 border border-county-green block"
+            type="email"
+            name="email"
+            placeholder="Email"
+            required
+          />
+        </label>
         <input type="hidden" name="_subject" value="New message from portfolio page" />
-        <textarea className="my-4 mx-6 p-2 text-county-green 2xl:text-2xl 2xl:p-4 border border-county-green" name="message" rows="10" placeholder="message" required />
+        <label htmlFor="message" className="my-4 mx-6">
+          <span className="sr-only">Message</span>
+          <textarea
+            id="message"
+            className="w-full p-2 text-county-green 2xl:text-2xl 2xl:p-4 border border-county-green"
+            name="message"
+            rows="10"
+            placeholder="message"
+            required
+          />
+        </label>
         <button
           className="2xl:text-2xl bg-county-green text-soapstone dark:bg-soapstone dark:text-county-green block w-fit
-            min-h-44px min-w-44px py-4 px-8 mx-auto my-4 cursor-pointer 2xl:px-12 2xl:py-6 2xl:my-4"
+            min-h-44px min-w-44px py-3 px-6 mx-auto my-4 cursor-pointer 2xl:px-10 2xl:py-6 2xl:my-4"
           type="submit"
         >
           Send
