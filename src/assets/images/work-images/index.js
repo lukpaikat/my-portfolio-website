7a25eb757b4b7a3ab067b5c8d7b026abb6268cf3
lukpaikat <mhasbisaputra5@gmail.com>
2022-05-15 07:06:25 +0800

    add medium and small images

--- src/assets/images/work-images/index.js
+++ src/assets/images/work-images/index.js
@@ -1,14 +1,20 @@
-import bookShelfImage from './bookshelf-lukpaikat.netlify.app_ 720.png';
-import justWeatherImage from './justweather-lukpaikat.netlify.app_.png';
-import etchASketchImage from './lukpaikat.github.io_etch-a-sketch_.png';
-import rockPaperScrissorsImage from './lukpaikat.github.io_rock-paper-scissors_.png';
-import nyam2FoodsImage from './nyam2-foods.netlify.app_.png';
+import bookShelfImage from './bookshelf-lukpaikat.netlify.app_medium.jpg';
+import bookShelfImageSmall from './bookshelf-lukpaikat.netlify.app_small.jpg';
+import justWeatherImage from './justweather-lukpaikat.netlify.app_medium.jpg';
+import justWeatherImageSmall from './justweather-lukpaikat.netlify.app_small.jpg';
+import etchASketchImage from './lukpaikat.github.io_etch-a-sketch_medium.jpg';
+import etchASketchImageSmall from './lukpaikat.github.io_etch-a-sketch_small.jpg';
+import rockPaperScrissorsImage from './lukpaikat.github.io_rock-paper-scissors_medium.jpg';
+import rockPaperScrissorsImageSmall from './lukpaikat.github.io_rock-paper-scissors_small.jpg';
+import nyam2FoodsImage from './nyam2-foods.netlify.app_medium.jpg';
+import nyam2FoodsImageSmall from './nyam2-foods.netlify.app_small.jpg';
 
 const workImagesData = [
   {
     name: 'Nyam2Foods',
     description: 'A dummy restaurant catalogue',
     image: nyam2FoodsImage,
+    imageSmall: nyam2FoodsImageSmall,
     url: 'https://nyam2-foods.netlify.app/',
     codeUrl: 'https://gitfront.io/r/lukpaikat/ee2191cca7399d6c71abf246ede3ea5188962119/Nyam2-foods/',
   },
@@ -16,6 +22,7 @@ const workImagesData = [
     name: 'BookShelf',
     description: 'Book reading manager webapp',
     image: bookShelfImage,
+    imageSmall: bookShelfImageSmall,
     url: 'https://bookshelf-lukpaikat.netlify.app/',
     codeUrl: 'https://gitfront.io/r/lukpaikat/9977b1958325696f728d9f000310b6baf54a04b8/bookshelf/',
   },
@@ -23,6 +30,7 @@ const workImagesData = [
     name: 'JustWeather',
     description: 'Weather forecast webapp',
     image: justWeatherImage,
+    imageSmall: justWeatherImageSmall,
     url: 'https://justweather-lukpaikat.netlify.app/',
     codeUrl: 'https://gitfront.io/r/lukpaikat/8ec706a533347fdfee6ec86f19782a4171f8d5aa/JustWeather/',
   },
@@ -30,6 +38,7 @@ const workImagesData = [
     name: 'etch-a-sketch',
     description: 'pixel-by-pixel drawing webapp',
     image: etchASketchImage,
+    imageSmall: etchASketchImageSmall,
     url: 'https://lukpaikat.github.io/etch-a-sketch/',
     codeUrl: 'https://github.com/lukpaikat/etch-a-sketch',
   },
@@ -37,6 +46,7 @@ const workImagesData = [
     name: 'Rock Paper Scissors',
     description: 'Rock Paper Scissors webapp game',
     image: rockPaperScrissorsImage,
+    imageSmall: rockPaperScrissorsImageSmall,
     url: 'https://lukpaikat.github.io/rock-paper-scissors/',
     codeUrl: 'https://github.com/lukpaikat/rock-paper-scissors',
   },
