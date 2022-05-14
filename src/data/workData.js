abe1ce64abe192fe41e9d5916092869be317a31c
lukpaikat <mhasbisaputra5@gmail.com>
2022-05-15 07:50:21 +0800

    create separate data source for work data

---
+++ src/data/workData.js
@@ -0,0 +1,46 @@
+import workImages from '../assets/images/work-images';
+
+const workData = [
+  {
+    name: 'Nyam2Foods',
+    description: 'A dummy restaurant catalogue',
+    image: workImages.nyam2Foods.medium,
+    imageSmall: workImages.nyam2Foods.small,
+    url: 'https://nyam2-foods.netlify.app/',
+    codeUrl: 'https://gitfront.io/r/lukpaikat/ee2191cca7399d6c71abf246ede3ea5188962119/Nyam2-foods/',
+  },
+  {
+    name: 'BookShelf',
+    description: 'Book reading manager webapp',
+    image: workImages.bookShelf.medium,
+    imageSmall: workImages.bookShelf.small,
+    url: 'https://bookshelf-lukpaikat.netlify.app/',
+    codeUrl: 'https://gitfront.io/r/lukpaikat/9977b1958325696f728d9f000310b6baf54a04b8/bookshelf/',
+  },
+  {
+    name: 'JustWeather',
+    description: 'Weather forecast webapp',
+    image: workImages.justWeather.medium,
+    imageSmall: workImages.justWeather.small,
+    url: 'https://justweather-lukpaikat.netlify.app/',
+    codeUrl: 'https://gitfront.io/r/lukpaikat/8ec706a533347fdfee6ec86f19782a4171f8d5aa/JustWeather/',
+  },
+  {
+    name: 'etch-a-sketch',
+    description: 'pixel-by-pixel drawing webapp',
+    image: workImages.etchASketch.medium,
+    imageSmall: workImages.etchASketch.small,
+    url: 'https://lukpaikat.github.io/etch-a-sketch/',
+    codeUrl: 'https://github.com/lukpaikat/etch-a-sketch',
+  },
+  {
+    name: 'Rock Paper Scissors',
+    description: 'Rock Paper Scissors webapp game',
+    image: workImages.rockPaperScissors.medium,
+    imageSmall: workImages.rockPaperScissors.small,
+    url: 'https://lukpaikat.github.io/rock-paper-scissors/',
+    codeUrl: 'https://github.com/lukpaikat/rock-paper-scissors',
+  },
+];
+
+export default workData;
