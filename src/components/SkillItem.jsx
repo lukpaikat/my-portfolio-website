d1067edef292008872947ff7fc1c28dc099ee5b3
lukpaikat <mhasbisaputra5@gmail.com>
2022-05-15 11:35:19 +0800

    fix skillItem key

--- src/components/SkillItem.jsx
+++ src/components/SkillItem.jsx
@@ -10,11 +10,10 @@ function SkillItem(props) {
     triggerOnce: true,
     fallbackInView: true,
   });
-  const { key, logo, name } = props;
+  const { logo, name } = props;
   return (
     <div
       ref={ref}
-      key={key}
       className={`mx-2 h-24 w-24 lg:h-28 lg:w-28 xl:h-36 xl:w-36 2xl:h-56 2xl:w-56 border my-2
       border-county-green dark:border-soapstone flex flex-col items-center justify-center
       ${inView ? 'animate__animated animate__fadeInUp' : ''} opacity-0`}
@@ -30,7 +29,6 @@ function SkillItem(props) {
 
 SkillItem.propTypes = {
   logo: PropTypes.string.isRequired,
-  key: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
 };
 
