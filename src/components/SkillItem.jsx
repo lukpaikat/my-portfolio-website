43e6195e7b765c0cefa43826ed9b20850cddd6d6
lukpaikat <mhasbisaputra5@gmail.com>
2022-05-15 11:08:46 +0800

    add SkillItem comp and animate them

---
+++ src/components/SkillItem.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useInView } from 'react-intersection-observer';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Icon } from '@iconify/react';
+import PropTypes from 'prop-types';
+
+function SkillItem(props) {
+  const { ref, inView } = useInView({
+    threshold: 0,
+    triggerOnce: true,
+    fallbackInView: true,
+  });
+  const { key, logo, name } = props;
+  return (
+    <div
+      ref={ref}
+      key={key}
+      className={`mx-2 h-24 w-24 lg:h-28 lg:w-28 xl:h-36 xl:w-36 2xl:h-56 2xl:w-56 border my-2
+      border-county-green dark:border-soapstone flex flex-col items-center justify-center
+      ${inView ? 'animate__animated animate__fadeInUp' : ''} opacity-0`}
+    >
+      <Icon
+        className="h-4/6 w-4/6 m-auto\"
+        icon={logo}
+      />
+      <p className="text-center mt-2 text-xs 2xl:text-xl">{name}</p>
+    </div>
+  );
+}
+
+SkillItem.propTypes = {
+  logo: PropTypes.string.isRequired,
+  key: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default SkillItem;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { Icon } from '@iconify/react';
+import { useInView } from 'react-intersection-observer';
+import SkillItem from './SkillItem';
 
 function Skills() {
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+    triggerOnce: true,
+    fallbackInView: true,
+  });
   const iconItems = [
     ['vscode-icons:file-type-html', 'logoHtml5', 'HTML5'],
     ['vscode-icons:file-type-css', 'logoCss3', 'CSS3'],
@@ -12,22 +17,26 @@ function Skills() {
     ['logos:tailwindcss-icon', 'logoTailWindCss', 'Tailwind CSS'],
   ];
   const iconElements = iconItems.map(([logo, key, name]) => (
-    <div
+    <SkillItem
       key={key}
-      className="mx-2 h-24 w-24 lg:h-28 lg:w-28 xl:h-36 xl:w-36 2xl:h-56 2xl:w-56 border my-2
-      border-county-green dark:border-soapstone flex flex-col items-center justify-center"
-    >
-      <Icon
-        className="h-4/6 w-4/6 m-auto\"
-        icon={logo}
-      />
-      <p className="text-center mt-2 text-xs 2xl:text-xl">{name}</p>
-    </div>
+      logo={logo}
+      name={name}
+    />
   ));
   return (
-    <section id="skills" className="mt-16 px-4 md:h-screen md:max-h-[400px] xl:max-h-[700px] md:flex md:flex-col md:justify-center">
-      <h2 className="mb-4 mx-6 text-5xl xl:text-6xl 2xl:text-8xl 2xl:mb-12 text-center">SKILLS</h2>
-      <p className="2xl:text-2xl mx-6 text-center">These are the technologies I&apos;ve worked with</p>
+    <section ref={ref} id="skills" className="mt-16 px-4 md:h-screen md:max-h-[400px] xl:max-h-[700px] md:flex md:flex-col md:justify-center">
+      <h2
+        className={`mb-4 mx-6 text-5xl xl:text-6xl 2xl:text-8xl 2xl:mb-12 text-center
+          ${inView ? 'animate__animated animate__fadeInDown' : ''} opacity-0`}
+      >
+        SKILLS
+      </h2>
+      <p
+        className={`2xl:text-2xl mx-6 text-center
+        ${inView ? 'animate__animated animate__fadeInDown' : ''} opacity-0`}
+      >
+        These are the technologies I&apos;ve worked with
+      </p>
       <div className="flex flex-wrap w-full justify-center mt-8 px-6">
         {iconElements}
       </div>
